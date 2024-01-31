fb01fc84070ef15ae22b6465c26ac8d7aed1587a
Mark Sweat <mark.sweat@gmail.com>
2024-01-31 08:25:37 -0600

    Update description in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
         'openai>=1.7.2',
         'python-dotenv>=1.0.0'
     ],
-  description = 'pcvt is a simple command line implementation of prompt chain variation testing for OpenAI and Azure OpenAI',
+  description = 'pcvt is a tool to scale-test small changes to a chat prompt chain for OpenAI and Azure OpenAI GPT backends',
   version = '0.1.108',
   author = 'Mark Sweat',
   author_email = 'mark@surfkansas.com',
